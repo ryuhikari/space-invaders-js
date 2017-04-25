d8fd8d628f5b49ecc62ee4f64811c23ade1f3b56
Ryuhikari <fernandovillab@gmail.com>
2017-04-25 22:29:27 +0200

    Arrange and fix the code

--- js/main.js
+++ js/main.js
@@ -1,17 +1,6 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var canvasContainer = document.getElementById("canvas-container");
-
-// GLOBALS
-const SCALE_VALUE = 3;
-const SCALE_VALUE_UFOS = 2;
-const UFOS_PER_ROW = 11;
-const N_ROWS = 5;
-
-const canvasWidth = canvas.width;
-const canvasHeight = canvas.height;
-const canvasRatio = canvasWidth / canvasHeight;
-
+/**
+ * Resize the canvas to expand the parent preserving aspect ratio
+ */
 function resize() {
     const canvasContainerWidth = canvasContainer.offsetWidth;
     const canvasContainerHeight = canvasContainer.offsetHeight;
@@ -32,251 +21,47 @@ window.addEventListener("orientationchange", resize);
 
 resize();
 
-////////////////////////////////////////////////
-///                Player Ship               ///
-////////////////////////////////////////////////
+function createEnemies(ufosPerRow, numRows, spaceX, spaceY, offsetX, offsetY, design) {
+    var enemies = [];
 
-// Playership model, drawn through a matrix
-const spaceship = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
-    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-];
-
-// Ship drawing function, given a matrix
-function drawShips(shipToDraw, color, offsetX, offsetY){
-    for(var i = 0; i < shipToDraw.length; i++){
-        for(var j = 0; j < shipToDraw[i].length; j++){
-            if(shipToDraw[i][j]){
-                ctx.fillStyle = color;
-                ctx.fillRect(j + offsetX, i + offsetY, 1, 1);
-            }
-        }
-    }
-}
-
-// Playership object constructor
-function PlayerShip(){
-    this.x = canvas.width/2;
-    this.y = canvas.height-40;
-    this.color = "#33FF00";
-
-    this.draw = function(){
-        ctx.scale(SCALE_VALUE, SCALE_VALUE);
-        drawShips(spaceship, this.color, this.x/SCALE_VALUE, this.y/SCALE_VALUE);
-        ctx.scale(1/SCALE_VALUE, 1/SCALE_VALUE);
-    }
-}
-
-var ship = new PlayerShip();
-
-//Control of playership with mouse (horizontal movement)
-//canvas.style.cursor = "none";
-canvas.addEventListener("mousemove", function(e){
-    ship.x = e.pageX - canvas.offsetLeft - 25;
-});
-
-////////////////////////////////////////////////
-///                 Enemy Ships              ///
-////////////////////////////////////////////////
-
-//Matrix with the enemy ships models
-
-const ufo1a = [
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-];
-
-const ufo1b = [
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-];
-
-const ufo2a = [
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0],
-    [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
-    [0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0],
-    [0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-];
-
-const ufo2b = [
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0],
-    [0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0],
-    [0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0],
-    [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-];
-
-const ufo3a = [
-    [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
-    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
-    [0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0],
-    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0],
-    [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-];
-
-const ufo3b = [
-    [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
-    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
-    [0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0],
-    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
-    [0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-];
-
-const ufo4a = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
-    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
-    [0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-    [0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-];
-
-// Variables containing the 4 types of ships and their alternative forms
-const ufo1 = [ufo1a, ufo1b];
-const ufo2 = [ufo2a, ufo2b];
-const ufo3 = [ufo3a, ufo3b];
-const ufo4 = [ufo4a, ufo4a];
-
-const enemyUfos = [ufo1, ufo2, ufo3, ufo4];
-const colorUfos = ["white", "white", "white", "red"];
-const points = [10, 20, 30, 100];
-
-// Enemy ships object constructor.
-// The arrays above give the extra information needed to draw the ships
-var UfoShip = function(ufo, color, offsetX, offsetY, points){
-    this.x = offsetX;
-    this.y = offsetY;
-    this.mode = 0;
-    this.ufo = ufo;
-    this.height = 5;
-    this.color = color;
-    this.offsetX = offsetX;
-    this.offsetY = offsetY;
-
-    this.alive = true;
-    this.dirX = 1;
-    this.velX = 0.5;
-
-    this.counter = 0;
-
-    this.moveDownwards = function(){
-        this.y += this.height;
-        this.dirX *= -1;
-    }
-
-    this.move = function(){
-        this.counter++;
-        this.x = this.x + this.dirX*this.velX;
-        if(this.counter >= 15){
-            this.counter = 0;
-            this.mode++;
-        }
-        if(this.mode > 1){
-            this.mode = 0;
+    for(var i = 0; i < numRows; i++){
+        var positionY = i * spaceY + offsetY
+        var selectUfo = design[i];
+        for(var j = 0; j < ufosPerRow; j++){
+            var positionX = j * spaceX + offsetX;
+            var enemyUfo = new UfoShip(enemyUfos[selectUfo].model, enemyUfos[selectUfo].color, positionX, positionY, enemyUfos[selectUfo].points);
+            enemies.push(enemyUfo);
         }
     }
 
-    this.die = function(){
-        this.alive = false;
-    }
-
-    this.draw = function(){
-        drawShips(this.ufo[this.mode], this.color, this.x, this.y);
-    }
-    /*// If enemy hits playership, game over automatically
-    this.crash = function(player){
-
-    }
-    */
+    return enemies;
 }
 
-// Grid to place the enemy ships
-var ufoRowPosition = [];
-for(var i = 0; i<UFOS_PER_ROW; i++){
-    ufoRowPosition.push(i*20+20);
-}
-var gridRows = [];
-for(var i = 0; i<N_ROWS; i++){
-    gridRows.push(i*10+30);
+var enemies = createEnemies(11, 5, 20, 15, 20, 30, [0, 1, 1, 2, 2]);
+
+// Function to draw every alive enemy in the array
+function drawUfos(enemies){
+    ctx.save();
+    ctx.scale(SCALE_VALUE_UFOS, SCALE_VALUE_UFOS);
+
+    for(var i = 0; i < enemies.length; i++){
+        enemies[i].draw();
+    }
+
+    ctx.restore();
 }
 
-// Enemies array
-var enemies = [];
-var selectUfo = 0;
-var enemyUfo;
-
-for(var i = 0; i<gridRows.length; i++){
-    for(var j = 0; j<ufoRowPosition.length; j++){
-        enemyUfo = new UfoShip(enemyUfos[selectUfo], colorUfos[selectUfo], ufoRowPosition[j], gridRows[i], points[selectUfo]);
-        enemies.push(enemyUfo);
-    }
-    if(i%2 - 1 ){
-        selectUfo++;
-    }
-    if(selectUfo > 2){
-        selectUfo = 0;
-    }
-}
- // Function to draw every alive enemy in the array
-function drawUfos(){
-    for(var i = 0; i<enemies.length; i++){
-        if(enemies[i].alive){
-            enemies[i].draw();
-        }
-    }
-}
-
-// Function to move every enemy as a block (when at least one of them gets to the edge of the board, move downwards and reverse direction)
+// Function to move every enemy as a block (when at least one of them gets to the edge of the board,
+// move downwards and reverse direction)
 var changeDir = 0;
-function moveUfos(){
-    for(var i = 0; i<enemies.length; i++){
+function moveUfos(enemies){
+    for (var i = 0; i < enemies.length; i++){
         enemies[i].move();
-        if(enemies[i].x >= canvas.width/SCALE_VALUE_UFOS-15 || enemies[i].x <= 0){
+        if (enemies[i].x >= canvas.width/SCALE_VALUE_UFOS-15 || enemies[i].x <= 0){
             changeDir = 1;
         }
     }
-    if(changeDir){
+    if (changeDir){
         for(var j = 0; j<enemies.length; j++){
             enemies[j].moveDownwards();
         }
@@ -288,29 +73,13 @@ function moveUfos(){
 ///                 Animation                ///
 ////////////////////////////////////////////////
 
-var ufoMode = 0;
-var counter = 0;
-
 // Function update, to animate the game
 function update(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ship.draw();
-
-    ctx.scale(SCALE_VALUE_UFOS, SCALE_VALUE_UFOS);
-    drawUfos();
-    ctx.scale(1/SCALE_VALUE_UFOS, 1/SCALE_VALUE_UFOS);
-
-    moveUfos();
-
-    counter++;
-    if(counter >=15){
-        ufoMode++;
-        if(ufoMode > 1){
-            ufoMode = 0;
-        }
-        counter = 0;
-    }
-
+    drawUfos(enemies);
+    moveUfos(enemies);
+    
     ID = requestAnimationFrame(update);
 }
 
---
+++ js/objects.js
@@ -0,0 +1,239 @@
+/**
+ * Canvas
+ */
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var canvasContainer = document.getElementById("canvas-container");
+
+/**
+ * Configuration
+ */
+const SCALE_VALUE = 3;
+const SCALE_VALUE_UFOS = 2;
+const UFOS_PER_ROW = 11;
+const N_ROWS = 5;
+
+const canvasWidth = canvas.width;
+const canvasHeight = canvas.height;
+const canvasRatio = canvasWidth / canvasHeight;
+
+/**
+ * Ship drawing function, given a matrix
+ */
+function drawShip(shipToDraw, color, offsetX, offsetY){
+    for(var i = 0; i < shipToDraw.length; i++){
+        for(var j = 0; j < shipToDraw[i].length; j++){
+            if(shipToDraw[i][j]){
+                ctx.fillStyle = color;
+                ctx.fillRect(j + offsetX, i + offsetY, 1, 1);
+            }
+        }
+    }
+}
+
+/**
+ * Playership object constructor
+ */
+function PlayerShip(){
+    this.x = canvas.width/2;
+    this.y = canvas.height;
+    this.color = "#33FF00";
+
+    this.spaceship = [
+	    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+	    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+	    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+	    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+	    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+	    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	];
+
+    this.draw = function(){
+    	ctx.save();
+        ctx.scale(SCALE_VALUE, SCALE_VALUE);
+        drawShip(this.spaceship, this.color, this.x/SCALE_VALUE, this.y/SCALE_VALUE);
+        ctx.restore();
+    }
+}
+
+var ship = new PlayerShip();
+
+/**
+ * Control of playership with mouse (horizontal movement)
+ */
+//canvas.style.cursor = "none";
+canvas.addEventListener("mousemove", function(e){
+    ship.x = e.pageX - canvas.offsetLeft - 25;
+
+});
+
+/**
+ * Enemy ships
+ */
+
+//Matrices with the enemy ships models
+
+const ufoModel1 = [
+	[
+	    [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	],
+
+	[
+	    [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	],
+];
+
+const ufoModel2 = [
+	[
+	    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0],
+	    [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+	    [0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0],
+	    [0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+	    [0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	],
+
+	[
+	    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0],
+	    [0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0],
+	    [0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0],
+	    [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+	    [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	],
+];
+
+const ufoModel3 = [
+	[
+	    [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+	    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+	    [0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0],
+	    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+	    [0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0],
+	    [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	],
+
+	[
+	    [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+	    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+	    [0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0],
+	    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+	    [0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	],
+];
+
+const ufoModel4 = [
+	[
+	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+	    [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+	    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+	    [0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0],
+	    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+	    [0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0],
+	    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	],
+];
+
+/**
+ * Ufo type constructor to hold the model, color and points of each enemy
+ */
+function UfoType(model, color, points) {
+	this.model = model;
+	this.color = color;
+	this.points = points;
+}
+
+// Create all the enemies
+const ufo1 = new UfoType(ufoModel1, "white", 10);
+const ufo2 = new UfoType(ufoModel2, "white", 20);
+const ufo3 = new UfoType(ufoModel3, "white", 30);
+const ufo4 = new UfoType(ufoModel4, "red", 100);
+
+// Hold the enemies in an array
+const enemyUfos = [ufo1, ufo2, ufo3, ufo4];
+
+/**
+ * Enemy ships object constructor
+ * 
+ * ufo: array with 2 ufo models to draw
+ * color: filling color
+ * offsetX: X position on canvas
+ * offsetY: Y position on canvas
+ * points: value to increment the player score if killed
+ */
+var UfoShip = function(ufo, color, offsetX, offsetY, points){
+    this.x = offsetX;
+    this.y = offsetY;
+    this.mode = 0;
+    this.ufo = ufo;
+    this.height = 5;
+    this.color = color;
+    this.offsetX = offsetX;
+    this.offsetY = offsetY;
+
+    this.dirX = 1;
+    this.velX = 0.5;
+
+    this.frames = 15;
+    this.counter = 0;
+
+    this.moveDownwards = function(){
+        this.y += this.height;
+        this.dirX *= -1;
+    }
+
+    this.move = function(){
+        this.counter++;
+        this.x = this.x + this.dirX*this.velX;
+        if (this.counter >= this.frames){
+            this.counter = 0;
+            this.mode++;
+        }
+        if (this.mode > 1){
+            this.mode = 0;
+        }
+    }
+
+    this.draw = function(){
+        drawShip(this.ufo[this.mode], this.color, this.x, this.y);
+    }
+    /*// If enemy hits playership, game over automatically
+    this.crash = function(player){
+
+    }
+    */
+}
